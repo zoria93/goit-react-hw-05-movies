39a4f591cfae15e8f746ede798438e4cca0b76f5
zoria93 <artemzora4@gmail.com>
2023-04-14 20:48:15 +0300

    Update Player.jsx

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -4,7 +4,8 @@ const Player = ({ title }) => {
   return (
     <>
       <Iframe
-        url={`https://v1681339303.bazon.site/embed?search=${title}`}
+        url={title}
+        src={`https://v1681494159.bazon.site/embed?search=${title}`}
         width="640px"
         height="320px"
         id=""
