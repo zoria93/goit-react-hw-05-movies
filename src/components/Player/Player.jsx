946611546feadc50495a1826d284dcab73f3187b
zoria93 <artemzora4@gmail.com>
2023-04-14 22:18:40 +0300

    Update Player.jsx

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,33 +1,33 @@
 import Iframe from 'react-iframe';
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 // import { getSearchTitle } from 'services/api';
-// import { getDetails } from 'services/api';
-// import { useParams } from 'react-router-dom';
+import { getDetails } from 'services/api';
+import { useParams } from 'react-router-dom';
 
-const Player = ({ title }) => {
-  // const [movies, setMovies] = useState([]);
-  // const { movieId } = useParams();
+const Player = () => {
+  // console.log(title);
+  const [movies, setMovies] = useState([]);
+  const { movieId } = useParams();
 
-  // useEffect(() => {
-  //   getDetails(movieId)
-  //     .then(movieDetails => setMovies(movieDetails))
-  //     .catch(error => console.log(error.message));
-  // }, [movieId]);
+  useEffect(() => {
+    getDetails(movieId)
+      .then(movieDetails => setMovies(movieDetails))
+      .catch(error => console.log(error.message));
+  }, [movieId]);
 
-  // const { title } = movies ?? '';
+  const { title } = movies ?? '';
 
   return (
     <>
-      <Iframe
-        url={`https://v1681494159.bazon.site/embed?search=${title}`}
-        src={`https://v1681494159.bazon.site/embed?search=${title}`}
-        width="640px"
-        height="320px"
-        id=""
-        className=""
-        display="block"
-        position="relative"
-      />
+      <iframe
+        src={`https://v1681499138.bazon.site/embed?search=${title}`}
+        frameborder="0"
+        allowfullscreen=""
+        referrerpolicy="origin"
+        width="800"
+        height="452"
+        title="iframe"
+      ></iframe>
     </>
   );
 };
